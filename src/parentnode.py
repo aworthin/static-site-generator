9df832cc8512c7c275c110c13c38e34718198723
Andy Worthington <bellyofthefish@icloud.com>
2025-08-22 21:43:09 -0500

    ParentNode

---
+++ src/parentnode.py
@@ -0,0 +1,20 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children == None:
+            raise ValueError("invalid HTML: no children")
+        props = self.props_to_html()
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+
+        return f"<{self.tag}{props}>{children_html}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
